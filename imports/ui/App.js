26e0233e294fbf349f0a2602bd2613b0beff8378
Anders Arnholm <anders@arnholm.se>
2018-03-18 11:32:45 +0100

    Initial style and content
    * Use http://materializecss.com
    * First css
    * Change data into personal-details
    * Remove Tasks and Todo.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react';
 
-import Task from './Task.js';
-
-// App component - represents the whole app
-export default class App extends Component {
-    getTasks() {
-        return [
-            { _id: 1, text: 'This is task 1' },
-            { _id: 2, text: 'This is task 2' },
-            { _id: 3, text: 'This is task 3' },
-        ];
+// PersonInfo component - represents the whole app
+export default class PersonInfo extends Component {
+    getName() {
+        return "ARNHOLM, Per Anders Niklas"
     }
-
-    renderTasks() {
-        return this.getTasks().map((task) => (
-            <Task key={task._id} task={task} />
-    ));
-    }
-
     render() {
         return (
-            <div className="container">
+            <div className="container person-view">
             <header>
-            <h1>Todo List</h1>
-        </header>
+                    <h2 className="header">{this.getName()}</h2>
+            </header>
+                <div className="container">
+                    <h3 className="header">Personliga Detaljer</h3>
+                    <table className="compactTable"><tbody>
+                    <tr><th colSpan="3" className="tableInfo">Grundfakta</th></tr>
+                    <tr><th>Fullständigt namn</th><td colSpan="2">ARNHOLM, Per Anders Niklas</td></tr>
+                    <tr><th>Födelsekön</th><td colSpan="2">Man</td></tr>
+                    <tr><th>Född</th><td>1972-09-20</td><td>Mölndal</td></tr>
+                    <tr><th colSpan="3" className="tableInfo">Andra Fakta</th></tr>
+                    <tr><th>Utbildning</th><td>Datavetenskap, Umeå Universitet</td></tr>
+                    <tr><th>Sysselsättning</th><td>IT-konsult</td></tr>
+                    <tr><th>Boende</th><td>1972	Mölndal</td></tr>
+                    <tr><th>Boende</th><td>ca. 1972 - ca. 1975</td><td>Kållered</td></tr>
+                    <tr><th>Boende</th><td>ca. 1975 - ca. 1980</td><td>Källby, Björgårdsvägen 19G</td></tr>
+                    <tr><th>Boende</th><td>ca. 1980 - ca. 1984</td><td>Staffanstorp, Strindbergs stig 1</td></tr>
+                    <tr><th>Boende</th><td>1984 - 1991</td><td>Mölndal, Södermalmsgatan 13</td></tr>
+                    <tr><th>Boende</th><td>1991 - Est. 1993</td><td>Umeå, Istidsgatan</td></tr>
+                    <tr><th>Boende</th><td>Est. 1993 - 1998</td><td>Umeå, Kandidatvägen 7</td></tr>
+                    <tr><th>Boende</th><td>1998 - 7 Maj 2012</td><td>Göteborg, Solstrålegatan</td></tr>
+                    <tr><th>Boende</th><td>7 Maj 2012</td><td>Svanesund, Björkvägen 9</td></tr>
+                    <tr><th>ID-Nummer</th><td>1</td></tr>
+                    </tbody></table>
 
-        <ul>
-        {this.renderTasks()}
-    </ul>
-        </div>
+                </div>
+            </div>
     );
     }
 }
