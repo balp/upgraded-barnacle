76d6c6be81feeb71a04f1f8595ef201bd7d16b04
Anders Arnholm <anders@arnholm.se>
2018-03-18 21:12:50 +0100

    Take name from Database

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Facts } from '../api/facts.js'
+import { Names } from "../api/names";
 import Fact from './Facts.js'
 
 // PersonInfo component - represents the whole app
 class PersonInfo extends Component {
     getName() {
-        return "ARNHOLM, Per Anders Niklas"
+        var surname = '';
+        var given = '';
+        for (n in this.props.names) {
+            let name = this.props.names[n]
+            if ('Surname' in name) {
+                surname = name['Surname']
+            }
+            if ('Given' in name) {
+                given = name['Given']
+            }
+            if ('Familiar' in name) {
+                given = name['Familiar']
+            }
+        }
+        return (<span>
+            <span>{surname}</span>, <span>{given}</span>
+        </span>)
     }
     renderFacts() {
         return this.props.facts.map((fact) => (
@@ -34,7 +51,9 @@ class PersonInfo extends Component {
 
 export default withTracker(() => {
     Meteor.subscribe('facts');
+    Meteor.subscribe('names');
     return {
-      facts: Facts.find({"ReferenceID":"PERS1"}).fetch(),
+        facts: Facts.find({"ReferenceID":"PERS1"}).fetch(),
+        names: Names.find({"PersonID":"PERS1"}).fetch(),
     };
 })(PersonInfo)
