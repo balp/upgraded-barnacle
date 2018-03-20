acb5762d2ef107deed27381703cdfa3f2ad1a500
Anders Arnholm <anders@arnholm.se>
2018-03-20 11:52:56 +0100

    List family information form database

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,6 +6,7 @@ import { Facts } from '../api/facts.js'
 import { Names } from "../api/names";
 import Fact from './Facts.js'
 import {Children} from "../api/children";
+import {Families} from "../api/families";
 
 // PersonInfo component - represents the whole app
 class PersonInfo extends React.Component {
@@ -15,23 +16,23 @@ class PersonInfo extends React.Component {
         }
         if (this.props.mynamesExists) {
             let name = this.props.mynames[0];
-            console.log("getPersonName(): ", name);
+            // console.log("getPersonName(): ", name);
             return (PersonInfo.formatName(name))
         }
         return (<span>UNKNOWN {this.props.id}</span>)
     }
     renderFacts() {
-        if (this.props.factsLoading) {
-            return("[Loading]")
+        if (this.props.myfactsLoading) {
+            return(<tr><th>"[Loading]"</th></tr>)
         }
-        if (this.props.factsExists) {
-            return this.props.facts.map((fact) => (
+        if (this.props.myfactsExists) {
+            return this.props.myfacts.map((fact) => (
                 <Fact key={fact._id} fact={fact}/>
             ));
         }
     }
     static formatName(name) {
-        console.log(name);
+        // console.log(name);
         let surname = !!name['Surname'] ? name['Surname'] : '';
         let given = !!name['Given'] ? name['Given'] : '';
         let formated_name = !!name['Familiar']
@@ -54,7 +55,7 @@ class PersonInfo extends React.Component {
 
     renderParents() {
         return this.props.selfchilds.map((child) =>
-        (<div key={child._id} className="col s12 family_parents">{console.log(child)}
+        (<div key={child._id} className="col s12 family_parents">
             <span>Parents:&nbsp;</span>
             <span className="family_person">{this.getName(child.Parent1Relation.ParentID)}</span>
             <span>&nbsp;&&nbsp;</span>
@@ -69,33 +70,55 @@ class PersonInfo extends React.Component {
             let personId = this.props.id;
             let siblings = this.props.childs.filter(child => child.FamilyID === familyId
                 && child.PersonID !== personId);
-            console.log(familyId, siblings);
+            // console.log(familyId, siblings);
             return siblings.map( sibling => (
                 <div key={sibling._id} className="col s11 offset-s1 siblings">{this.getName(sibling.PersonID)}</div>
             ))
         }
         return(<div className="col s11 offset-s1 siblings">[Loading]</div>)
     }
+    renderFamilyFacts(family) {
+        if (this.props.factsExists) {
+            let familyId = family.ID;
+            let facts = this.props.facts.filter(fact => fact.ReferenceID === familyId);
+            console.log(facts);
+            return facts.map((fact) => (
+                <Fact key={fact._id} fact={fact}/>
+            ));
+
+        }
+        return(<tr><td>[Loading]</td></tr>);
+    }
+    renderFamilyInfo() {
+        if (this.props.familiesExists ) {
+            let personId = this.props.id;
+            let families = this.props.families.filter(family => family.PrimeID === personId
+                || family.PartnerID === personId);
+            console.log(families);
+            return families.map( family => (
+                <div key={family._id} className="col s11 offset-s1 self_partner">
+                    <div className="col s12 family_self">
+                        <span className="family_person">{this.getName(family.PrimeID)}</span>
+                        <span>&nbsp;&&nbsp;</span>
+                        <span className="family_person">{this.getName(family.PartnerID)}</span>
+                    </div>
+                    <div className="col s11 offset-s1">
+                        <table className="compactTable"><tbody>
+                            {this.renderFamilyFacts(family)}
+                        </tbody></table>
+
+                    </div>
+
+                </div>
+            ));
+        }
+        return(<div className="col s11 offset-s1 self_partner" />)
+    }
     renderCloseFamily() {
         return(<div className="row">
             {this.renderParents()}
             {this.renderSiblings()}
-            <div className="col s11 offset-s1 self_partner">
-                <div className="row">
-                    <div className="col s12 family_self">
-                        <span className="family_person">ARNHOLM, Per Anders Niklas</span>
-                        <span>&nbsp;&&nbsp;</span>
-                        <span className="family_person">NORMARK, Linda</span>
-                    </div>
-                    <div className="col s2 offset-s1">Förlovning</div>
-                    <div className="col s2">21 Aug 1999</div>
-                    <div className="col s7">GKSS båthamn i Långedrag, Göteborg</div>
-                    <div className="col s2 offset-s1">ID-Nummer</div>
-                    <div className="col s9">45</div>
-                    <div className="col s2 offset-s1">Bevis</div>
-                    <div className="col s9">N542</div>
-                </div>
-            </div>
+            {this.renderFamilyInfo()}
         </div>)
     }
     render() {
@@ -126,9 +149,9 @@ PersonInfo.propTypes = {
     mynamesLoading: PropTypes.bool,
     mynamesExists: PropTypes.bool,
 
-    facts: PropTypes.array,
-    factsLoading: PropTypes.bool,
-    factsExists: PropTypes.bool,
+    myfacts: PropTypes.array,
+    myfactsLoading: PropTypes.bool,
+    myfactsExists: PropTypes.bool,
 
     selfchilds: PropTypes.array,
     selfchildsLoading: PropTypes.bool,
@@ -142,6 +165,13 @@ PersonInfo.propTypes = {
     childsLoading: PropTypes.bool,
     childsExists: PropTypes.bool,
 
+    families: PropTypes.array,
+    familiesLoading: PropTypes.bool,
+    familiesExists: PropTypes.bool,
+
+    facts: PropTypes.array,
+    factsLoading: PropTypes.bool,
+    factsExists: PropTypes.bool,
 
 };
 
@@ -151,34 +181,44 @@ export default withTracker(({ id }) => {
     const mynames_person = Names.find({"PersonID": id});
     const mynamesExists = !mynamesLoading && !!mynames_person;
 
-    const factsHandle = Meteor.subscribe('facts.person', id);
-    const factsLoading = !factsHandle.ready();
-    const facts_person = Facts.find({"ReferenceID": id});
-    const factsExists = !factsLoading && !!facts_person;
+    const myfactsHandle = Meteor.subscribe('facts.person', id);
+    const myfactsLoading = !myfactsHandle.ready();
+    const myfacts_person = Facts.find({"ReferenceID": id});
+    const myfactsExists = !myfactsLoading && !!myfacts_person;
 
     const selfchildsHandle = Meteor.subscribe('children.person', id);
     const selfchildsLoading = !selfchildsHandle.ready();
     const selfchilds_person = Children.find({"PersonID": id});
     const selfchildsExists = !selfchildsLoading && !!selfchilds_person;
 
-    const namesHandle = Meteor.subscribe('names', );
+    const namesHandle = Meteor.subscribe('names');
     const namesLoading = !namesHandle.ready();
     const names_person = Names.find();
     const namesExists = !namesLoading && !!names_person;
 
-    const childsHandle = Meteor.subscribe('children', id);
+    const childsHandle = Meteor.subscribe('children');
     const childsLoading = !childsHandle.ready();
     const childs_person = Children.find();
     const childsExists = !childsLoading && !!childs_person;
+
+    const familiesHandle = Meteor.subscribe('families');
+    const familiesLoading = !familiesHandle.ready();
+    const families_person = Families.find();
+    const familiesExists = !familiesLoading && !!families_person;
+
+    const factsHandle = Meteor.subscribe('facts', id);
+    const factsLoading = !factsHandle.ready();
+    const facts_person = Facts.find();
+    const factsExists = !factsLoading && !!facts_person;
 
     return {
         id,
         mynames: mynames_person.fetch(),
         mynamesLoading: mynamesLoading,
         mynamesExists: mynamesExists,
-        facts: facts_person.fetch(),
-        factsLoading,
-        factsExists,
+        myfacts: myfacts_person.fetch(),
+        myfactsLoading,
+        myfactsExists,
         selfchilds: selfchilds_person.fetch(),
         selfchildsLoading,
         selfchildsExists,
@@ -188,5 +228,11 @@ export default withTracker(({ id }) => {
         childs: childs_person.fetch(),
         childsLoading,
         childsExists,
+        families: families_person.fetch(),
+        familiesLoading,
+        familiesExists,
+        facts: facts_person.fetch(),
+        factsLoading,
+        factsExists,
     };
 })(PersonInfo)
